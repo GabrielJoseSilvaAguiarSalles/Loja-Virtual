fb3ac35eb5e8ba7e5f6fafdb7031074bceb0a3c8
GabrielJoseSilvaAguiarSalles <gabriel030202@gmail.com>
2026-09-14 21:13:29 -0300

    Removendo pycache, venv, db.sqlite3

--- backend/Produtos/admin.py
+++ backend/Produtos/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 from .models import Produto
 admin.site.register(Produto)
+#As entidades Clientes e Produtos possuem um relacionamento de N:N, porque um cliente pode comprar diversos produtos, assim como um mesmo tipo de produto pode ser
+#comprado por diversos clientes.
